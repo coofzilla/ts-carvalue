93be929132d8727ba21f7b2bd2744ee61cc1426d
Jay Hernandez <jeric614@yahoo.com>
2021-10-08 10:06:23 +0900

    implemented update w/entity; note builtin update method uses object/hooks wont execute automatically

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -9,6 +9,14 @@ export class UsersService {
     @InjectRepository(User)
     private repo: Repository<User>,
   ) {}
+  // @Injectable()
+  // export class UsersService {
+  //   repo: Repository<User>;
+
+  //   constructor(repo: Repository<User>) {
+  //     this.repo = repo;
+  //   }
+  // }
 
   create(email: string, password: string) {
     const user = this.repo.create({
@@ -18,12 +26,32 @@ export class UsersService {
 
     return this.repo.save(user);
   }
-}
-// @Injectable()
-// export class UsersService {
-//   repo: Repository<User>;
 
-//   constructor(repo: Repository<User>) {
-//     this.repo = repo;
-//   }
-// }
+  findOne(id: number) {
+    return this.repo.findOne(id);
+  }
+
+  find(email: string) {
+    return this.repo.find({ email });
+  }
+  //not using update method because built in update works with object
+  //if not working with entity hooks won't be called
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    console.log('original user', user);
+    Object.assign(user, attrs);
+    console.log('assigned', user);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+    return this.repo.remove(user);
+  }
+}
