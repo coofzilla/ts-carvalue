0a15a5e73aaf00e41ab1dc89989f9b1413e4494f
Jay Hernandez <jeric614@yahoo.com>
2021-10-11 16:05:29 +0900

    test f/user logged in

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -3,25 +3,51 @@ import { AuthService } from './auth.service';
 import { User } from './user.entity';
 import { UsersService } from './users.service';
 
-it('can create an instance of auth service', async () => {
-  //create fake copy of users service
-  const fakeUsersService: Partial<UsersService> = {
-    //only defined find/create because only ones used by auth serv
-    find: () => Promise.resolve([]),
-    create: (email: string, password: string) =>
-      //as type assertion
-      Promise.resolve({ id: 1, email, password } as User),
-  };
+describe('AuthService', () => {
+  let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
 
-  const module = await Test.createTestingModule({
-    providers: [
-      AuthService,
-      //if ask f/value of provide, give value from useValue
-      { provide: UsersService, useValue: fakeUsersService },
-    ],
-  }).compile();
+  beforeEach(async () => {
+    //create fake copy of users service
+    fakeUsersService = {
+      //only defined find/create because only ones used by auth serv
+      find: () => Promise.resolve([]),
+      create: (email: string, password: string) =>
+        //as type assertion
+        Promise.resolve({ id: 1, email, password } as User),
+    };
 
-  const service = module.get(AuthService);
+    const module = await Test.createTestingModule({
+      providers: [
+        AuthService,
+        //if ask f/value of provide, give value from useValue
+        { provide: UsersService, useValue: fakeUsersService },
+      ],
+    }).compile();
 
-  expect(service).toBeDefined();
+    service = module.get(AuthService);
+  });
+
+  it('can create an instance of auth service', async () => {
+    expect(service).toBeDefined();
+  });
+
+  it('creates a new user with a salted and hashed password', async () => {
+    const user = await service.signup('test@test.com', 'asdf');
+
+    expect(user.password).not.toEqual('asdf');
+    const [salt, hash] = user.password.split('.');
+    expect(salt).toBeDefined();
+    expect(hash).toBeDefined();
+  });
+
+  it('throws an error if user signs up with email that is in use', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([{ id: 1, email: 'a', password: '1' } as User]);
+    try {
+      await service.signup('test@test.com', 'asdf');
+    } catch (err) {
+      console.log('error is', err);
+    }
+  });
 });
