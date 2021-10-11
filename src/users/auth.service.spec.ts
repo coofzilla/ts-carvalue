86960df50e285abe9731281be8a1e62e9ca21575
Jay Hernandez <jeric614@yahoo.com>
2021-10-11 21:19:51 +0900

    test f/correct pw

--- src/users/auth.service.spec.ts
+++ src/users/auth.service.spec.ts
@@ -50,8 +50,40 @@ describe('AuthService', () => {
   });
 
   it('throws if signin is called with an unused email', async () => {
-    await expect(service.signin('ffff@ffff.com', 'ffff')).rejects.toThrow(
-      'user not found',
+    await expect(
+      service.signin('signup@123.com', 'mypassword'),
+    ).rejects.toThrow('user not found');
+  });
+
+  //if pw resolved, signin pw, or message wrong, will throw correct error message
+  //if all three correct, will resolve and "fail" test
+  it('throws if an invalid pw is provided, async', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([
+        {
+          id: 1,
+          email: 'a',
+          password:
+            '1b9984be05bb9e3d.84b015294bd71ee90e2ad1bd8215635fb67b9e4d80358421a4a78ea2a8013343',
+        } as User,
+      ]);
+    //remove PASS to make "fail", all three would be correct
+    await expect(service.signin('email', 'mypasswordPASS')).rejects.toThrow(
+      'bad password',
     );
+  });
+
+  it('returns a user if correct password is provided', async () => {
+    fakeUsersService.find = () =>
+      Promise.resolve([
+        {
+          id: 1,
+          email: 'bbbb@b.com',
+          password:
+            '1b9984be05bb9e3d.84b015294bd71ee90e2ad1bd8215635fb67b9e4d80358421a4a78ea2a8013343',
+        } as User,
+      ]);
+    const user = await service.signin('bbbb@b.com', 'mypassword');
+    expect(user).toBeDefined();
   });
 });
