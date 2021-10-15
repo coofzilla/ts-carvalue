9451f331c6b4ef9e1a7025a02d76e2a81a33b543
Jay Hernandez <jeric614@yahoo.com>
2021-10-15 12:00:59 +0900

    fixed all bugs w/orm f/test/dev environments

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -3,7 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateReportDto } from './dtos/create-report.dto';
 import { Report } from './report.entity';
-import { User } from 'src/users/user.entity';
+import { User } from '../users/user.entity';
 import { GetEstimateDto } from './dtos/get-estimate.dto';
 
 @Injectable()
